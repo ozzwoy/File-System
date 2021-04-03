77858ee43b2b364fb3ffba45fac0aef41941048e
ozzwoy <dima.pashenko.200081@gmail.com>
2021-04-03 13:42:32 +0300

    Corrected conversion utils

--- FileSystem/file_system/utils/Utils.h
+++ FileSystem/file_system/utils/Utils.h
@@ -2,22 +2,24 @@
 
 namespace Utils {
 
-    unsigned bytesToUint(const char* bytes, int size) {
-        int number = 0;
+    char* intToBytes(int number) {
+        char* bytes = new char[sizeof(int)];
 
-        for(int i = 0; i < size; i++ )
-            ((unsigned char*)(&number))[i] = ((unsigned char*)bytes)[i];
-
-        return number;
-    }
-
-    char* uintToBytes(unsigned number) {
-        char* bytes = new char[sizeof(unsigned)];
-
-        for(int i = 0; i < sizeof(unsigned); i++ )
+        for (int i = 0; i < sizeof(int); i++) {
             ((unsigned char*)bytes)[i] = ((unsigned char*)(&number))[i];
+        }
 
         return bytes;
+    }
+
+    int bytesToInt32(const char* bytes) {
+        int number = 0;
+
+        for (int i = 0; i < 4; i++) {
+            ((unsigned char*)(&number))[i] = ((unsigned char*)bytes)[i];
+        }
+
+        return number;
     }
 
 }
--- FileSystem/tests/test.cpp
+++ FileSystem/tests/test.cpp
@@ -2,7 +2,7 @@
 #include "../file_system/utils/Utils.h"
 
 int main() {
-    char* bytes = Utils::uintToBytes(5);
-    std::cout << Utils::bytesToUint(bytes, 1) << std::endl;
+    char* bytes = Utils::intToBytes(-236862);
+    std::cout << Utils::bytesToInt32(bytes) << std::endl;
     return 0;
 }
