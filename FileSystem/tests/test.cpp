a3ddfb300c62b1b79772ee0f214509dabc5068cc
DarynaBond <35077916+WAG13@users.noreply.github.com>
2021-04-04 21:52:41 +0300

    added tests

--- FileSystem/tests/test.cpp
+++ FileSystem/tests/test.cpp
@@ -1,6 +1,25 @@
-#include <iostream>
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
 
-int main() {
-    std::cout << "Hello World!" << std::endl;
-    return 0;
+#include "doctest.h"
+#include <cstring>
+#include "../file_system/entities/BitMap.h"
+
+TEST_CASE("File System"){
+
+    SUBCASE("BitMap"){
+        char buffer[9];
+        buffer[8] = '\0';
+        BitMap bitmap;
+        bitmap.parse("abcdefgh");
+
+        REQUIRE( bitmap.isBitSet(1) );
+
+        bitmap.setBitValue(1, 0);
+        bitmap.copyBytes(buffer);
+        REQUIRE( strcmp(buffer,"!bcdefgh") == 0 );
+
+        bitmap.setBitValue(1, 1);
+        bitmap.copyBytes(buffer);
+        REQUIRE( strcmp(buffer,"abcdefgh") == 0 );
+    }
 }
