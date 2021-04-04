8ce27ab78b179ea43464c10d133485e8bcf40789
yaroslava-l <54927782+yaroslava-l@users.noreply.github.com>
2021-04-04 12:07:17 +0300

    Add files via upload

---
+++ file_system/FileSystem.h
@@ -0,0 +1,23 @@
+#pragma once
+
+class FileSystem {
+private:
+	
+	//OFT oft;
+
+public:
+
+	FileSystem();
+	~FileSystem();
+
+	int createFile(const char* file_name);
+	int destroyFile(const char* file_name);
+	int open(const char* file_name);
+	int close(int index);
+	int read(int index, void* mem_area, int count);
+	int write(int index, void* mem_area, int count);
+	int lseek(int index, int pos);
+
+};
+
+
